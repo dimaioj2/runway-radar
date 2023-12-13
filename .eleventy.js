917b3388c934fd3b5b36301530b04b6df168a11c
John DiMaio <143565695+dimaioj2@users.noreply.github.com>
2023-12-13 01:05:11 +0000

    adds node env check

--- .eleventy.js
+++ .eleventy.js
@@ -2,15 +2,19 @@ const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 
 module.exports = function(eleventyConfig) {
+  let p = {}
+  if (process.env.NODE_ENV === 'production') {
+    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+    p={ pathPrefix: "/runway-radar/"}
+  }
   
-  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
     eleventyConfig.addPassthroughCopy("src/assets");
     eleventyConfig.addWatchTarget("./src/");
   
     
     // Return your Object options:
     return {
-      pathPrefix: "/runway-radar/",
+     ...p,
       dir: {
         input: "src",
         output: "docs",
